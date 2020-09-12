21904239306ed0669b70a57541cc0307f4467d4d
Ramadhana Bhanuharya Wishnumurti <wishnumurti312@gmail.com>
2020-09-12 22:53:47 +0700

    first push

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            newItem:"",
+            list:[]
+        }
+    }
+
+    updateInput(key, value){
+        //update react state
+    this.setState({
+        [key]: value
+    })
+    }
+
+    addItem(){
+        //create item with unique id
+        const newItem={
+            id: 1 + Math.random(),
+            value: this.state.newItem.slice()
+        };
+        
+        //copy of current list of items
+        const list = [...this.state.list];
+
+        // add new item to the list
+        list.push(newItem);
+
+        //update state with new list and reset newItem input
+        this.setState({
+            list,
+            newItem:"" 
+        });
+    }
+    deleteItem(id){
+        //copy current list of items
+        const list = [...this.state.list];
+
+        //filter out utem being deleted
+        const updatedList = list.filter(item => item.id !== id);
+        
+        this.setState({list: updatedList})
+    }
+    
+    render() {
+        return(
+            <div className = "App">
+                <div>
+                Add an Item...
+                <br/>
+                    <input
+                    type="text"
+                    placeholder="Type item here...."
+                    value = {this.state.newItem}
+                    onChange={e => this.updateInput("newItem", e.target.value)}
+                />
+                <button
+                onClick = {() => this.addItem()}>
+            
+                Add
+
+                </button>
+                <br/>
+                <ul>
+                    {this.state.list.map(item => {
+                        return(
+                            <li key={item.id}>
+                                {item.value}
+                                <button className="btn btn-floating" onClick={() => this.deleteItem(item.id)}>
+                    <i class="material-icons">x</i>
+                  </button>
+                            </li>
+                        )
+                    })}
+                </ul>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
+
+
+
+
+
+
